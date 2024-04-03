3c5bfc37a0f4fc2c3fd7238d970333d2ed3ec9d0
TheSmoothWater <thesmoothwater@gmail.com>
2024-04-03 14:20:23 -0400

    Done 100%

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -4,10 +4,46 @@ public class Recitation7 {
      * but you may NOT change the signatures of the methods below.
      */
     public static boolean lastItemReachable(int[] hopList){
-        return false; // replace this line with your code
+        if ((hopList.length == 1)){
+            return true;
+        }
+        int hop = hopList[0];
+        boolean result = true;
+        int previousHop = 0;
+        while(result){
+            if(hop == hopList[hopList.length - 1]){
+                return result;
+            }
+            else if(previousHop + hop >= hopList.length - 1 || hop == 0){
+                result = false;
+            }
+            else {
+                previousHop = hop + previousHop;
+                hop = hopList[hop];
+            }
+        }
+        return result;
+
     }
 
     public static int[] slidingWindowMin(int[] numberList, int windowSize){
-        return new int[0]; // replace this line with your code
+        int[] maxNumbers = new int[numberList.length - windowSize + 1];
+        if(windowSize > numberList.length){
+            return new int[0];
+        }
+        int rightWindow;
+        int min = numberList[0];
+        for(int leftWindow = 0; leftWindow < numberList.length - windowSize + 1; leftWindow ++){
+            rightWindow = leftWindow + windowSize - 1;
+            min = numberList[leftWindow];
+            for(int i = leftWindow; i < rightWindow + 1; i++){
+                if(min > numberList[i]){
+                    min = numberList[i];
+                }
+            }
+            maxNumbers[leftWindow] = min;
+        }
+
+        return maxNumbers;
     }
 }
